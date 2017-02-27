a8ccbab8798cbc81b27c6e869a2d41c615546cef
senator0 <rurkpd@hi2.in>
2017-02-27 18:08:23 +0330

    Create config.lua

---
+++ bot/config.lua
@@ -0,0 +1,40 @@
+do local _ = {
+  enabled_plugins = {
+    "addrem",
+    "bc",
+    "supergroup",
+    "msg_checks",
+    "pin",
+    "owner",
+    "online",
+    "plugins",
+    "admin",
+    "id",
+    "del",
+    "clean",
+    "expiretime",
+    "filter",
+    "setlink",
+    "setrules",
+     "help",
+     "leave",
+     "welcome",
+     "lock_bots",
+    "me"
+  },
+  group  = {
+    data = "bot/group.json"
+  },
+  sudo_users = {
+    170146015, --sudo
+    204507468,
+    196568905,
+    309970083
+  },
+robot = {
+    376973363,--bot
+    0
+  }
+}
+return _
+end
