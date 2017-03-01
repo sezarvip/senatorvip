cfde7d5a3b1af83bd3b177381f30a5b71c8f69bc
sezarvip <sezartel.tg@gmail.com>
2017-03-01 17:14:32 +0330

    aria

--- plugins/leave.lua
+++ plugins/leave.lua
@@ -3,12 +3,12 @@ local group = load_data('bot/group.json')
 local addgroup = group[tostring(msg.chat_id)]
 	if matches[1] == 'leave' and is_sudo(msg) and addgroup or not addgroup then
 	      --tg.changeChatMemberStatus(chat, robot, 'Left')
-		  tg.changeChatMemberStatus(msg.chat_id_, 0, 'Left') 
+		  tg.changeChatMemberStatus(msg.chat_id_, 372557073, 'Left') 
     end
 end	
 return {
   patterns = {
-   "^[/#!](leave)$",
+   "^[/#!](rem)$",
   },
   run = run
 }
