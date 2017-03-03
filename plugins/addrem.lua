6966853907d8fda5bec286185f97c0154d6f87f0
sezarvip <sezartel.tg@gmail.com>
2017-03-03 20:53:51 +0330

    aria

--- plugins/addrem.lua
+++ plugins/addrem.lua
@@ -34,9 +34,9 @@ mute_audio = "no"
                   }
       }
       save_data(_config.group.data, group)
-tg.sendMessage(msg.chat_id, msg.id_, 1, 'SuperGroup 💈💈 added\n*Channel:* @sezarinfo', 1)
+tg.sendMessage(msg.chat_id, msg.id_, 1, 'سـوپرگـروه در لیـست گـروهای ربـات مـوجود است\n*Channel:* @sezarinfo', 1)
 else
-tg.sendMessage(msg.chat_id, msg.id_, 1, 'SuperGroup 💈💈 add\n*Channel:* @sezarinfo', 1)
+tg.sendMessage(msg.chat_id, msg.id_, 1, 'سـوپرگـروه درلیست گـروهای ربـات اضافه شـد\n*Channel:* @sezarinfo', 1)
 end
 end
 local function remgroup(msg)
@@ -45,9 +45,9 @@ local groupa = group[tostring(msg.chat_id)]
 if groupa then
 group[tostring(msg.chat_id)] = nil
       save_data(_config.group.data, group)
-tg.sendMessage(msg.chat_id, msg.id_, 1, 'SuperGroup 💈💈 removed\n*Channel:* @sezarinfo', 1)
+tg.sendMessage(msg.chat_id, msg.id_, 1, 'سـوپرگـروه درلیست گروهای ربـات موجود نیسـت\n*Channel:* @sezarinfo', 1)
 else
-tg.sendMessage(msg.chat_id, msg.id_, 1, 'SuperGroup 💈💈 remove\n*Channel:* @sezarinfo', 1)
+tg.sendMessage(msg.chat_id, msg.id_, 1, 'سـوپرگـروه ازلیست گروهای ربـات حذف شـد\n*Channel:* @sezarinfo', 1)
 end
 end
 
